048f5f372eae07ef7eca73eb4b3c0fd709a2459d
Alex Shearn <alex@isthat.it>
2013-12-26 23:47:39 +0000

    wiping for switch to scala

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ocamlfind ocamlopt -o trawl -linkpkg -package json-static,netclient -syntax camlp4o trawl.ml
--- trawl.ml
+++
@@ -1,8 +0,0 @@
-(*
-This program will effectively be a 'blacklist' checker, searching 
-specific files for specific words.
-First steps in OCaml!
-*)
-
-let result = Http_client.Convenience.http_get("http://127.0.0.1:5984/trawl");;
-print_endline result;;
